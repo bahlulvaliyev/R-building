1c3a7b164d0e8f7ce35f45236fba6cfea88a7e65
Behlul <bvaliyev96@gmail.com>
2018-12-07 19:59:34 +0400

    [+] day last commit

--- js/preload.js
+++ js/preload.js
@@ -1,5 +1,32 @@
-$(document).ready(function () {
-    $('.section-two').on('mouseneter',()=>{
-        $('.cut-thr-img-1').animate({width: 0})
-    })
-});
+var myMap;
+
+ymaps.ready(init);
+
+function init () {
+    myMap = new ymaps.Map('map', {
+        center: [40.38, 49.82],
+        zoom: 10
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+}
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: ["<span class='next' > -</span>Next", "Prev<span class='prev'> -</span>"],
+    navClass: ['owl-prev', 'owl-next'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+    
+})
